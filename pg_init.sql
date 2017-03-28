03f2ecfb84c0038c12eec3b41267479127330ef3
Giulio Roman <giulio.roman@eurac.edu>
2017-03-29 00:56:25 +0200

    create decent index.php

--- pg_init.sql
+++
@@ -1,5 +0,0 @@
-create database example;
-create role user with password 'bingo';
-create database example;
-grant all privileges on database example to user;
-alter role example with login;
--- src/index.php
+++ src/index.php
@@ -1,5 +1,26 @@
-<?php
+<?php include('../postgresql-www-data.php'); ?>
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Hello World!</title>
+	</head>
+	<body>
+		<?php
 
-phpinfo();
+			// test db connection by listing all tables
+			$db = new PDO("pgsql:host={$DBCONFIG['HOST']};dbname={$DBCONFIG['NAME']}", $DBCONFIG['USER'], $DBCONFIG['PASS']);
+			$stmt = $db->query('SELECT * FROM pg_catalog.pg_tables');
+			$res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			
+			// print results
+			echo "<pre>";
+			var_dump($res);
+			echo "</pre>";
 
-?>
+			// print php general info
+			phpinfo();
+			
+		?>		
+	</body>
+</html>
