e78a227f8d7f96c7f2f80bfae0ce51f8549d7fc2
Giulio Roman <giulio.roman@eurac.edu>
2017-05-21 15:04:44 +0200

    validate checkout form

--- src/util.js
+++ src/util.js
@@ -14,3 +14,11 @@ function animateCartIcon() {
 		$('#navbar-toggle-btn').toggleClass('pulsating');
 	}, 700);
 }
+
+function onlyNumericInput(e) {
+	var key_codes = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 0, 8];
+
+	if (!($.inArray(e.which, key_codes) >= 0)) {
+		e.preventDefault();
+	}
+}
